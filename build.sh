6c3058939d80fd42faa56ad862d1a3985342a769
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2025-03-23 03:33:23 +0100

    Revert "Attempt remove flatpak repo and add file"
    This reverts commit 4fc1b8be1ca60306f74aaddfbf5d886e0d154b93.

--- build.sh
+++ build.sh
@@ -12,7 +12,10 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y @virtualization
 dnf5 install -y gnome-shell-extension-pop-shell
-dnf5 install -y cascadia-code-nf-fonts
+
+dnf5 -y copr enable atim/starship
+dnf5 -y install starship
+dnf5 -y copr disable atim/starship
 
 # Use a COPR Example:
 #
@@ -21,24 +24,6 @@ dnf5 install -y cascadia-code-nf-fonts
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf5 -y copr enable atim/starship
-dnf5 -y install starship
-dnf5 -y copr disable atim/starship
-
-### Example for enabling a System Unit File
+#### Example for enabling a System Unit File
 
 systemctl enable libvirtd
-
-### Install flatpaks
-
-#Remove fedora remote
-flatpak remote-delete fedora
-
-#Add flathub
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak install org.mozilla.firefox
-
-### Write to files
-
-echo "eval \"\$(starship init bash)\"" >> /etc/bash.bashrc.local
