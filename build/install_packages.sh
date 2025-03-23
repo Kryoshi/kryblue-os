6f369d583c91586d6cf6aa7e83e35f68c27fb0f0
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2025-03-23 22:30:17 +0100

    Add gnome shell extensions

--- build/install_packages.sh
+++ build/install_packages.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 
-### curl
+### direct
 
 # Starship prompt
 curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
@@ -14,6 +14,26 @@ echo "eval \"\$(starship init bash)\"" >> /etc/bashrc
 
 ### dnf
 
-dnf5 install -y @virtualization
-dnf5 install -y gnome-shell-extension-pop-shell
-dnf5 install -y nerd-fonts
+packages = (
+  @virtualization
+  nerd-fonts
+  fastfetch
+)
+for package in ${packages[@]}; do
+  dnf5 install -y "$package"
+done
+
+# Gnome extensions
+extensions = (
+  gnome-shell-extension-pop-shell
+  gnome-shell-extension-blur-my-shell
+  gnome-shell-extension-caffeine
+  gnome-shell-extension-appindicator
+  gnome-shell-extension-vertical-workspaces
+  gnome-shell-extension-system-monitor-applet
+  gnome-shell-extension-freon
+  gnome-shell-extension-frippery-move-clock
+)
+for extension in ${extensions[@]}; do
+  dnf5 install -y "$extension"
+done
