f73da912daf3150a5a9876f0f1b1dc3bbb7411d1
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2025-03-23 18:49:42 +0100

    Disable no permission warning for build script

--- build/build.sh
+++ build/build.sh
@@ -8,7 +8,12 @@ set -ouex pipefail
 rsync -rvK /ctx/files/ /
 
 # Run build scripts
+
+# shellcheck disable=SC1091
 /ctx/build/copr_enable.sh
+# shellcheck disable=SC1091
 /ctx/build/install_packages.sh
+# shellcheck disable=SC1091
 /ctx/build/systemd.sh
+# shellcheck disable=SC1091
 /ctx/build/copr_disable.sh
