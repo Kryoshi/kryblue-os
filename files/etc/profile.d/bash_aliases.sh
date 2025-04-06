e396c84a62643834cde97292efd9e74ce471ca45
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2025-04-06 21:52:40 +0200

    Add bash aliases

---
+++ files/etc/profile.d/bash_aliases.sh
@@ -0,0 +1,8 @@
+alias pinta='flatpak run com.github.PintaProject.Pinta'
+alias drawio='flatpak run com.jgraph.drawio.desktop'
+alias nbstrip_jq="jq --indent 1 \
+    '(.cells[] | select(has(\"outputs\")) | .outputs) = []  \
+    | (.cells[] | select(has(\"execution_count\")) | .execution_count) = null  \
+    | .metadata = {\"language_info\": {\"name\": \"python\", \"pygments_lexer\": \"ipython3\"}} \
+    | .cells[].metadata = {} \
+    '"
