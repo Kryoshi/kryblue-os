3d6b67e7ce2efd2f8e60d2572d41e308f64705e5
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2025-04-30 00:00:26 +0200

    Attempt nvidia driver issue fix

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,11 @@ set -ouex pipefail
 # Copy system files onto image
 rsync -rvK /ctx/files/ /
 
+# Rebuild nvidia akmod
+dnf5 -y install akmods
+akmods --rebuild --force --akmod nvidia
+rpm -qi kmod-nvidia-$(uname -r)
+
 # Run build scripts
 /ctx/build/copr_enable.sh
 /ctx/build/install_packages.sh
